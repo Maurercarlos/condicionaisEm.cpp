e07fc4540e4d9fad0355a0b1477ae42a58588532
Maurer <162385501+Maurercarlos@users.noreply.github.com>
2024-04-08 22:29:25 +0000

    errei

---
+++ excercicio5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+
+    int num;
+
+    printf("Digite o seu número: ");
+    scanf("%d", &num);
+
+    if (num > 10) {
+        printf("Seu número é maior que 10\n");
+    } else if (num == 10) {
+    printf("Seu número é igual a 10\n");
+    } else  {
+        printf("")
+    }
+    return 0;
+}
